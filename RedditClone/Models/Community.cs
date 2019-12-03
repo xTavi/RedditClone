878e294de0b65a138e1cfd013278fe6c6eec43e0
Tavi <taviatwork@gmail.com>
2019-12-03 15:56:57 +0200

    Added the DbConnectionString

---
+++ RedditClone/Models/Community.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RedditClone.Models
+{
+    public class Community
+    {
+        public int CommunityId { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class CommunityDBContext : DbContext
+    {
+        public CommunityDBContext() : base("C:\USERS\CONST\DESKTOP\CACAT\REDDITCLONE\REDDITCLONE\APP_DATA\BAZASPATIALA.MDF") { }
+        public DbSet<Community> Communities { get; set; } // Not sure if this is working properly or the line above 
+    }
+}
