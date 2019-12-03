0c462bdd1a0bc989206f78bcce6ee36e0155cba4
Tavi <taviatwork@gmail.com>
2019-12-03 15:39:36 +0200

    Nou proiect care merge
    sa moara ma sa

---
+++ RedditClone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedditClone.Startup))]
+namespace RedditClone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
