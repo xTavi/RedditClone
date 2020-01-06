f48752b77d8d77236dd0ca1d7a6216108be956a7
Mihai Pure <39124927+MihaiPure@users.noreply.github.com>
2020-01-06 16:58:15 +0200

    added stuff to PostController

--- RedditClone/Controllers/PostController.cs
+++ RedditClone/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using RedditClone.Models;
+﻿using Microsoft.AspNet.Identity;
+using RedditClone.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,128 @@ namespace RedditClone.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        [Authorize(Roles = "Guest,User,Moderator,Administrator")]
+        [AllowAnonymous]
         // GET: 
         public ActionResult Index()
         {
-            
+            var posts = db.Posts.Include("User");
+
+            ViewBag.Posts = posts;
 
             return View();
+        }
+
+        [Authorize(Roles = "User,Moderator,Administrator")]
+        // GET: vizualizarea unei postari
+        public ActionResult Show(int id)
+        {
+            Post post = db.Posts.Find(id);
+
+
+            ViewBag.afisareButoane = false;
+            if (User.IsInRole("Editor") || User.IsInRole("Administrator"))
+            {
+                ViewBag.afisareButoane = true;
+            }
+
+            ViewBag.esteAdmin = User.IsInRole("Administrator");
+            ViewBag.utilizatorCurent = User.Identity.GetUserId();
+
+            return View(post);
+        }
+
+        [Authorize(Roles = "User,Moderator,Administrator")]
+        public ActionResult New()
+        {
+            Post post = new Post();
+            post.UserId = User.Identity.GetUserId();
+            return View(post);
+        }
+
+
+        // POST: trimitem datele catre server pentru creare
+        [HttpPost]
+        [Authorize(Roles = "User,Moderator,Administrator")]
+        public ActionResult New(Post post)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Posts.Add(post);
+                    db.SaveChanges();
+                    TempData["message"] = "O noua postare a fost adaugata!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["message"] = "Something went wrong!";
+                    return View(post);
+                }
+            }
+            catch (Exception e)
+            {
+                TempData["message"] = "Something went wrong!";
+                return View(post);
+            }
+        }
+
+        [Authorize(Roles = "Moderator,Administrator")]
+        // GET: vrem sa editam un student
+        public ActionResult Edit(int Id)
+        {
+            Post post = db.Posts.Find(Id);
+            ViewBag.Post = post;
+            if (post.UserId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+            {
+                return View(post);
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unei postari care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Moderator,Administrator")]
+        public ActionResult Edit(int id, Post requestPost)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Post post = db.Posts.Find(id);
+                    if (post.UserId == User.Identity.GetUserId() ||
+                        User.IsInRole("Administrator"))
+                    {
+                        if (TryUpdateModel(post))
+                        {
+                            post.Title = requestPost.Title;
+                            post.Content = requestPost.Content;
+                            db.SaveChanges();
+                            TempData["message"] = "Datele postarii au fost modificate!";
+                        }
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unei postari care nu va apartine!";
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                else
+                {
+                    return View(requestPost);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return View(requestPost);
+            }
         }
 
     }
